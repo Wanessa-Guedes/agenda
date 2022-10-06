faedda1c701b20111d09dbb0e2420574385c13da
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-10-06 10:15:10 -0300

    feat: adding transactions

--- back-end/src/repositories/contactsRepository.ts
+++ back-end/src/repositories/contactsRepository.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import { prisma } from "../config/database.js";
 import {
   CreateContact,
@@ -26,19 +27,35 @@ async function insertContact(
   contactData: CreateContact,
   contactInfoData: CreateInfos
 ) {
-  const contactId = await prisma.contact.create({
-    data: contactData,
-  });
+  await prisma.$transaction(async (othePrisma: PrismaClient) => {
+    const contactId = await prisma.contact.create({
+      data: contactData,
+    });
 
-  const dataInfo: CreateInfoContact = {
-    whatsapp: contactInfoData.whatsapp,
-    email: contactInfoData.email,
-    tel: contactInfoData.tel,
-    contactId: contactId.id,
-  };
+    if (!contactId) {
+      throw {
+        type: "not_found",
+        message: "No id found",
+      };
+    }
 
-  await prisma.contact_Info.create({
-    data: dataInfo,
+    const dataInfo: CreateInfoContact = {
+      whatsapp: contactInfoData.whatsapp,
+      email: contactInfoData.email,
+      tel: contactInfoData.tel,
+      contactId: contactId.id,
+    };
+
+    const contactInfos = await prisma.contact_Info.create({
+      data: dataInfo,
+    });
+
+    if (!contactInfos) {
+      throw {
+        type: "not_found",
+        message: "Contact not create",
+      };
+    }
   });
 }
 
@@ -46,55 +63,108 @@ async function updateContact(
   contactData: ContactById,
   contactUpdate: CreateInfos
 ) {
-  const contactId = await prisma.contact.findFirst({
-    where: {
-      id: contactData.id,
-      userId: contactData.userId,
-    },
-  });
+  await prisma.$transaction(async (othePrisma: PrismaClient) => {
+    const contactId = await prisma.contact.findFirst({
+      where: {
+        id: contactData.id,
+        userId: contactData.userId,
+      },
+    });
 
-  const contactInfoId = await prisma.contact_Info.findFirst({
-    where: {
-      contactId: contactId.id,
-    },
-  });
+    if (!contactId) {
+      throw {
+        type: "not_found",
+        message: "contact infos not found",
+      };
+    }
 
-  await prisma.contact_Info.update({
-    where: {
-      id: contactInfoId.id,
-    },
-    data: {
-      whatsapp: contactUpdate.whatsapp,
-      email: contactUpdate.email,
-      tel: contactUpdate.tel,
-    },
+    const contactInfoId = await prisma.contact_Info.findFirst({
+      where: {
+        contactId: contactId.id,
+      },
+    });
+
+    if (!contactInfoId) {
+      throw {
+        type: "not_found",
+        message: "contact infos not found",
+      };
+    }
+
+    const updateInfos = await prisma.contact_Info.update({
+      where: {
+        id: contactInfoId.id,
+      },
+      data: {
+        whatsapp: contactUpdate.whatsapp,
+        email: contactUpdate.email,
+        tel: contactUpdate.tel,
+      },
+    });
+
+    if (!updateInfos) {
+      throw {
+        type: "not_found",
+        message: "error to update infos",
+      };
+    }
   });
 }
 
 async function deleteContact(contactData: ContactById) {
-  const contactId = await prisma.contact.findFirst({
-    where: {
-      id: contactData.id,
-      userId: contactData.userId,
-    },
-  });
+  await prisma.$transaction(async (othePrisma: PrismaClient) => {
+    const contactId = await prisma.contact.findFirst({
+      where: {
+        id: contactData.id,
+        userId: contactData.userId,
+      },
+    });
 
-  const contactInfoId = await prisma.contact_Info.findFirst({
-    where: {
-      contactId: contactId.id,
-    },
-  });
+    if (!contactId) {
+      throw {
+        type: "not_found",
+        message: "contact infos not found",
+      };
+    }
 
-  await prisma.contact_Info.delete({
-    where: {
-      id: contactInfoId.id,
-    },
-  });
+    const contactInfoId = await prisma.contact_Info.findFirst({
+      where: {
+        contactId: contactId.id,
+      },
+    });
 
-  await prisma.contact.delete({
-    where: {
-      id: contactData.id,
-    },
+    if (!contactInfoId) {
+      throw {
+        type: "not_found",
+        message: "contact infos not found",
+      };
+    }
+
+    const deleteContactInfo = await prisma.contact_Info.delete({
+      where: {
+        id: contactInfoId.id,
+      },
+    });
+
+    if (!deleteContactInfo) {
+      throw {
+        type: "not_found",
+        message: "error to delete contact infos",
+      };
+    }
+
+    const deleteContact = await prisma.contact.delete({
+      where: {
+        id: contactData.id,
+      },
+    });
+
+    if (!deleteContact) {
+      throw {
+        type: "not_found",
+        message: "error to delete contact",
+      };
+    }
   });
 }
 
